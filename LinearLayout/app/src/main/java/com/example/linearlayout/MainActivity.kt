bd49d83d75561a124a1c59055c5cdf8a3257206c
A19AlbertoNS <a19albertons@iessanclemente.net>
2025-10-01 18:12:56 +0200

    Add Linear Layout

---
+++ LinearLayout/app/src/main/java/com/example/linearlayout/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.linearlayout
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import org.w3c.dom.Text
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val nombre = findViewById<TextView>(R.id.textView)
+        val escribible = findViewById<TextView>(R.id.textView3)
+        val button = findViewById<Button>(R.id.button)
+        button.setOnClickListener {
+            escribible.text = "Llamando a "+nombre.text
+        }
+    }
+}
