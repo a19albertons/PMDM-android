84869b1c1cebf62127300901280b59a2b8a5fb66
A19AlbertoNS <a19albertons@iessanclemente.net>
2025-10-17 21:26:27 +0200

    Fix some mistakes in FrameLayout and add MediaPlayer

--- FrameLayout/app/src/main/java/com/example/framelayout/MainActivity.kt
+++ FrameLayout/app/src/main/java/com/example/framelayout/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.example.framelayout
 
+import android.media.MediaPlayer
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    // Variable que maneja el reproductor de audio
+    private var mediaPlayer: MediaPlayer? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +22,31 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val imagenOursense = findViewById<ImageView>(R.id.imageView1)
+        val imagenSantiago = findViewById<ImageView>(R.id.imageView2)
+        val textoModficar = findViewById<TextView>(R.id.textView3)
+
+        imagenOursense.setOnClickListener {
+            textoModficar.text = getString(R.string.ourense_desc)
+            // Reprodución del audio
+            mediaPlayer?.release()
+            mediaPlayer = MediaPlayer.create(this, R.raw.audio)
+            mediaPlayer?.start()
+
+        }
+        imagenSantiago.setOnClickListener {
+            textoModficar.text = getString(R.string.santiago_desc)
+            // Reprodución del audio
+            mediaPlayer?.release()
+            mediaPlayer = MediaPlayer.create(this, R.raw.audio)
+            mediaPlayer?.start()
+        }
+    }
+
+    override fun onDestroy() {
+        // Destruye el reproductor de audio al destruir la actividad
+        super.onDestroy()
+        mediaPlayer?.release()
+        mediaPlayer = null
     }
 }
